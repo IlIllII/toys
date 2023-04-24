d7360cd3bdc23584b21ced3fa31c21144e3f1b94
IlIllII <78166995+IlIllII@users.noreply.github.com>
2023-04-24 19:26:03 -0400

    Generalize markov chain to handle variable length prefixes

--- markov/textGen.py
+++ markov/textGen.py
@@ -1,27 +1,44 @@
 import random
 
+N = 2
 
-def construct_prefix_map(filename: str):
+
+def make_starting_prefix_array(n: int = N):
+    return ["\n"] * N
+
+
+def make_key(*args):
+    key = ""
+    for arg in args:
+        key += str(arg)
+    return key
+
+
+def construct_prefix_map(filename: str) -> dict:
     prefix_map = dict()
-    first = second = "\n"
+    prefixes = make_starting_prefix_array()
     with open(filename) as f:
         for line in f.readlines():
             words = line.split()
             for word in words:
-                prefix_map[first + second] = prefix_map.get(first + second, []) + [word]
-                first, second = second, word
-    prefix_map[first + second] = prefix_map.get(first + second, []) + ["\n"]
+                prefix_map[make_key(*prefixes)] = prefix_map.get(make_key(*prefixes), []) + [word]
+                prefixes[:-1] = prefixes[1:]
+                prefixes[-1] = word
+    prefix_map[make_key(*prefixes)] = prefix_map.get(make_key(*prefixes), []) + ["\n"]
     return prefix_map
 
+
 def generate_text(n: int, prefix_map: dict):
-    first = second = "\n"
+    prefixes = make_starting_prefix_array()
     for i in range(n):
-        suffixes = prefix_map[first + second]
+        suffixes = prefix_map[make_key(*prefixes)]
         word = random.choice(suffixes)
         print(word, end=" ")
         if word == "\n":
-            first = second = "\n"
-        first, second = second, word
+            prefixes = make_starting_prefix_array()
+        prefixes[:-1] = prefixes[1:]
+        prefixes[-1] = word
+
 
 if __name__ == "__main__":
     prefix_map = construct_prefix_map("text.txt")
