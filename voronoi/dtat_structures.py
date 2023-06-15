cd6f7ca2ef9d348d4fb1dd000ec913a9b8f393e1
IlIllII <78166995+IlIllII@users.noreply.github.com>
2023-06-14 20:53:33 -0400

    Begin debugging Arc constructor

--- voronoi/dtat_structures.py
+++ voronoi/dtat_structures.py
@@ -2,13 +2,13 @@ from queue import PriorityQueue
 from events import SiteEvent, CircleEvent
 
 
-points = [(0, 0), (1, 1), (2, 2), (3, 3)]
+points = [(1, 0), (1, 1), (2, 0.5), (3, 3), (2, 2.5), (3, 2), (4, 1), (5, 0)]
 
 sweep_line = 0
 
 event_queue = PriorityQueue()
 for p in points:
-    event_queue.put(SiteEvent(p[0], p[1]))
+    event_queue.put((p[1], SiteEvent(p[0], p[1])))
 
 # while event_queue.qsize() > 0:
 #     event = event_queue.get()
@@ -44,10 +44,24 @@ class Node:
         self.right = node
         node.parent = self
     
+    def __repr__(self) -> str:
+        return f"Node({self.value})"
+    
+    def __str__(self) -> str:
+        return self.__repr__()
+    
 class Arc(Node):
     def __init__(self, value, left=None, right=None):
         super().__init__(value, left, right)
         self.site = value
+        self.circle_events = []
+    
+    def __repr__(self):
+        return f"Arc({self.site})"
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
 
 class BreakPoint(Node):
     def __init__(self, value, left=None, right=None):
@@ -125,8 +139,7 @@ class BreakPoint(Node):
         self.right_site = None
     
     def get_x(self):
-        # TODO compute intersection of parabolas
-        raise NotImplementedError
+        return self.left_site[0] + self.right_site[0] / 2
 
 class BinaryTree:
     def __init__(self):
@@ -142,29 +155,101 @@ class BinaryTree:
         return node
 
     def split_arc(self, arc, new_site):
-        breakpoint = BreakPoint()
-        breakpoint.left_site = arc.site
-        breakpoint.right_site = new_site
-        breakpoint.parent = arc.parent
-        arc.parent = breakpoint
-        breakpoint.left = arc
-        sub_breakpoint = BreakPoint()
-        sub_breakpoint.left_site = new_site
-        sub_breakpoint.right_site = arc.site
-        sub_breakpoint.parent = breakpoint
-        breakpoint.right = sub_breakpoint
-        left_arc = Arc(new_site)
-        left_arc.parent = sub_breakpoint
-        sub_breakpoint.left = left_arc
-        right_arc = Arc(arc.site)
-        right_arc.parent = sub_breakpoint
-        sub_breakpoint.right = right_arc
+        print("splitting arc")
+        print(f"arc={arc}")
+        print(f"arc_site={arc.site}")
+        print(f"new_site={new_site}")
+        if arc.site[0] <= new_site[0]:
+            print("arc site was a left site")
+            breakpoint = BreakPoint()
+            breakpoint.left_site = arc.site
+            breakpoint.right_site = new_site
+            if not arc.parent:
+                breakpoint.parent = arc.parent
+            elif arc.parent.left == arc:
+                breakpoint.parent = arc.parent
+                arc.parent.left = breakpoint
+            else:
+                breakpoint.parent = arc.parent
+                arc.parent.right = breakpoint
+            print(f"parent={breakpoint.parent}")
+            arc.parent = breakpoint
+            breakpoint.left = arc
+            sub_breakpoint = BreakPoint()
+            sub_breakpoint.left_site = new_site
+            sub_breakpoint.right_site = arc.site
+            sub_breakpoint.parent = breakpoint
+            breakpoint.right = sub_breakpoint
+            left_arc = Arc(new_site)
+            left_arc.parent = sub_breakpoint
+            sub_breakpoint.left = left_arc
+            right_arc = Arc(arc.site)
+            right_arc.parent = sub_breakpoint
+            sub_breakpoint.right = right_arc
+        else:
+            print("arc site was a right site")
+            breakpoint = BreakPoint()
+            breakpoint.left_site = new_site
+            breakpoint.right_site = arc.site
+            if not arc.parent:
+                breakpoint.parent = arc.parent
+            elif arc.parent.left == arc:
+                breakpoint.parent = arc.parent
+                arc.parent.left = breakpoint
+            else:
+                breakpoint.parent = arc.parent
+                arc.parent.right = breakpoint
+            arc.parent = breakpoint
+            breakpoint.left = arc
+            sub_breakpoint = BreakPoint()
+            sub_breakpoint.left_site = arc.site
+            sub_breakpoint.right_site = new_site
+            sub_breakpoint.parent = breakpoint
+            breakpoint.right = sub_breakpoint
+            left_arc = Arc(arc.site)
+            left_arc.parent = sub_breakpoint
+            sub_breakpoint.left = left_arc
+            right_arc = Arc(new_site)
+            right_arc.parent = sub_breakpoint
+            sub_breakpoint.right = right_arc
+        
+        assert breakpoint.left.parent == breakpoint
+        assert breakpoint.right.parent == breakpoint
+        assert sub_breakpoint.left.parent == sub_breakpoint
+        assert sub_breakpoint.right.parent == sub_breakpoint
+
+        if breakpoint.parent:
+            assert breakpoint.parent.left == breakpoint or breakpoint.parent.right == breakpoint
+        print(f"breakpoint={breakpoint}")
+        print(f"sub_breakpoint={sub_breakpoint}")
+        print(f"left_arc={left_arc}")
+        print(f"right_arc={right_arc}")
+
+        print("printing breakpoint tree")
+        self.print_tree(breakpoint)
+
+        print(f"breakpoint.parent={breakpoint.parent}")
+        print(f"sub_breakpoint.parent={sub_breakpoint.parent}")
+
+
+        if breakpoint.parent == None:
+            self.root = breakpoint
+        
+        current = breakpoint
+        while current.parent:
+            current = current.parent
+            
+
+        self.print_tree(self.root)
+        print([arc.site for arc in self.get_arcs()])
+        print(f"root={self.root}")
 
         # Add edge starts to cell table and vertex table? Could also do this when we finish...
         breakpoint.edge = HalfEdge()
         breakpoint.edge.start = [arc.site[0] + new_site[0] / 2, arc.site[1] + new_site[1] / 2]
         sub_breakpoint.edge = HalfEdge()
         sub_breakpoint.edge.start = [arc.site[0] + new_site[0] / 2, arc.site[1] + new_site[1] / 2]
+        self.scan_for_circle_events()
     
     def get_arcs(self):
         arcs = []
@@ -179,11 +264,13 @@ class BinaryTree:
         return arcs
 
     def scan_for_circle_events(self):
-        for i, arc in enumerate(self.get_arcs):
+        print("scanning for circle events")
+        arcs = self.get_arcs()
+        for i, arc in enumerate(arcs):
             try:
-                left_arc = self.get_arcs[i - 1]
+                left_arc = arcs[i - 1]
                 middle_arc = arc
-                right_arc = self.get_arcs[i + 1]
+                right_arc = arcs[i + 1]
             except IndexError:
                 continue
             if middle_arc.site[1] >= left_arc.site[1] and middle_arc.site[1] >= right_arc.site[1]:
@@ -191,11 +278,12 @@ class BinaryTree:
             else:
                 event = create_circle_event(left_arc, middle_arc, right_arc)
                 for arc in [left_arc, middle_arc, right_arc]:
-                    if not arc.circle_events:
+                    print(arc.circle_events)
+                    if len(arc.circle_events) == 0:
                         arc.circle_events = []
                     arc.circle_events.append(event)
                 if event:
-                    get_event_queue().put(event)
+                    get_event_queue().put((event.y, event))
     
     def remove_arc(self, x):
         arc = self.find_arc(x)
@@ -235,6 +323,12 @@ class BinaryTree:
             new_breakpoint.right_site = right_child.site
         else:
             new_breakpoint.right_site = right_child.left_site
+    
+    def print_tree(self, node, indent=0):
+        if node:
+            print(" " * indent + str(node))
+            self.print_tree(node.left, indent + 2)
+            self.print_tree(node.right, indent + 2)
         
 
         
@@ -277,16 +371,17 @@ class EdgeNode:
 
 
 def get_sweep_line():
-    raise NotImplementedError
+    return sweep_line
 
 def get_event_queue():
     return event_queue
 
 def create_circle_event(left, middle, right):
+    print("\033[2;31;43m creating circle event\033[0;0m")
     if not left or not right or not middle:
         return None
 
-    p0, p1, p2 = (left.x, left.y), (middle.x, middle.y), (right.x, right.y)
+    p0, p1, p2 = left.site, middle.site, right.site
     circle = circumcircle(p0, p1, p2)
     if not circle or circle[1] + circle[2] < get_sweep_line():
         return None
@@ -323,21 +418,24 @@ def circumcircle(p0, p1, p2):
     return x, y, radius
 
 def scan_and_remove_circle_events(event_queue, site_position):
-    for event in event_queue.queue:
-        if event.__class__ == CircleEvent:
+    for i, event in enumerate(event_queue.queue):
+        if event[1].__class__ == CircleEvent:
             circle_center = (event.x, event.y)
             circle_radius = event.radius
             distance = ((site_position[0] - circle_center[0]) ** 2 + (site_position[1] - circle_center[1]) ** 2) ** (1 / 2)
             if distance <= circle_radius:
-                event_queue.remove(event)
+                event_queue.queue.pop(i)
 
 voronoi_vertices = []
 
-def process_site_event(event, binary_tree):
+def process_site_event(event, binary_tree: BinaryTree):
     # Find arc above site
     arc = binary_tree.find_arc(event.x)
+    print("processing site event")
+    print(arc.site)
     # Split arc
     binary_tree.split_arc(arc, event.site)
+    print([arc.site for arc in binary_tree.get_arcs()])
     # Scan for circle events
     scan_and_remove_circle_events(get_event_queue(), (event.x, event.y))
 
@@ -355,14 +453,24 @@ def process_circle_event(event, binary_tree):
 
 binary_tree = BinaryTree()
 
+print(event_queue.queue)
 while event_queue.qsize() > 0:
     event = event_queue.get()
+    print(event)
+    event = event[1]
+    sweep_line = event.y
     if event.__class__ == SiteEvent:
         if binary_tree.root is None:
             binary_tree.root = Arc(event.site)
         else:
             process_site_event(event, binary_tree)
     elif event.__class__ == CircleEvent:
+        print("    \033[2;31;43m HIT A CIRCLE EVENT!!!!\033[0;0m")
         process_circle_event(event, binary_tree)
 
+print(binary_tree.get_arcs())
+for arc in binary_tree.get_arcs():
+    print(arc.site)
+sorted_arcs = sorted(binary_tree.get_arcs(), key=lambda arc: arc.site[0])
+print([arc.site for arc in sorted_arcs])
 print(voronoi_vertices)
