31214bdab17f0acec4de10e58f8631763ab15e5f
IlIllII <78166995+IlIllII@users.noreply.github.com>
2023-06-17 23:39:10 -0400

    Get rays bouncing correctly

--- raytrace/raytrace.py
+++ raytrace/raytrace.py
@@ -1,14 +1,20 @@
 import pygame
 import math
+import random
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 640
+MAX_DEPTH = 5
+
 
 class Vector3:
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
-    
+
     def length(self):
-        return (self.x ** 2 + self.y ** 2 + self.z ** 2) ** 0.5
+        return (self.x**2 + self.y**2 + self.z**2) ** 0.5
 
     def normalized(self):
         length = self.length()
@@ -18,13 +24,13 @@ class Vector3:
 
     def mult(self, scalar):
         return Vector3(self.x * scalar, self.y * scalar, self.z * scalar)
-    
+
     def dot(self, other):
         return self.x * other.x + self.y * other.y + self.z * other.z
 
     def __add__(self, other):
         return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
-    
+
     def __sub__(self, other):
         return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
 
@@ -34,7 +40,7 @@ class Plane:
         self.position = position
         self.normal = normal
         self.color = color
-    
+
     def intersect(self, ray):
         d = self.normal.dot(self.position)
         if self.normal.dot(ray.direction) == 0:
@@ -43,10 +49,9 @@ class Plane:
         if t < 0:
             return False
         return t
-    
+
     def get_normal(self, point):
         return self.normal
-
 
 
 class Sphere:
@@ -55,11 +60,10 @@ class Sphere:
         self.radius = radius
         self.color = color
 
-
     def intersect(self, ray):
         line_to_sphere = self.center - ray.origin
         dp = line_to_sphere.dot(ray.direction)
-        if dp < 0: # Ray points away from sphere
+        if dp < 0:  # Ray points away from sphere
             return False
         d_squared = line_to_sphere.dot(line_to_sphere) - dp * dp
         if d_squared > self.radius * self.radius:
@@ -68,10 +72,10 @@ class Sphere:
         near_intersect = dp - d
         far_intersect = dp + d
         return min(near_intersect, far_intersect)
-    
 
     def get_normal(self, point):
-        return Vector3(point.x - self.center.x, point.y - self.center.y, point.z - self.center.z).normalized()
+        return (point - self.center).normalized()
+
 
 class Light(Sphere):
     def __init__(self, center, radius, color):
@@ -80,9 +84,9 @@ class Light(Sphere):
 
 class Ray:
     def __init__(self, origin, direction):
-        self.origin = origin
+        self.origin = origin + direction.normalized().mult(0.001)
         self.direction = direction.normalized()
-    
+
     def get_point(self, t):
         x = self.origin.x + self.direction.x * t
         y = self.origin.y + self.direction.y * t
@@ -93,11 +97,13 @@ class Ray:
         d = self.direction
         n = normal
         n = n.mult(d.dot(n) * 2)
+        # n = n + Vector3(
+        #     random.random() * 0.1, random.random() * 0.1, random.random() * 0.1
+        # )
         return Ray(point, Vector3(d.x - n.x, d.y - n.y, d.z - n.z))
 
 
-
-def get_direction_to(pixel, from_position, image_width, image_height, fov=math.pi/3):
+def get_direction_to(pixel, from_position, image_width, image_height, fov=math.pi / 3):
     aspect_ratio = image_width / image_height
     x, y = pixel
     u = (2 * (x + 0.5) / image_width - 1) * aspect_ratio
@@ -107,11 +113,101 @@ def get_direction_to(pixel, from_position, image_width, image_height, fov=math.p
     return direction
 
 
+def get_color(pixel, objects, camera_position, max_depth, light):
+    if max_depth <= 0:
+        return (0, 0, 0)
+    intersects = []
+    normals = []
+    objs = []
+    color = (0, 0, 0)
+    ray_direction = get_direction_to(
+        pixel, camera_position, SCREEN_WIDTH, SCREEN_HEIGHT
+    )
+    for obj in objects:
+        intersect = obj.intersect(Ray(camera_position, ray_direction))
+        if intersect:
+            intersects.append(intersect)
+            normals.append(
+                obj.get_normal(Ray(camera_position, ray_direction).get_point(intersect))
+            )
+            objs.append(obj)
+
+    if len(intersects) > 0:
+        index = intersects.index(min(intersects))
+        intersect = intersects[index]
+        normal = normals[index]
+        point = Ray(camera_position, ray_direction).get_point(intersect)
+        color = objs[index].color
+
+        vec_to_light = Vector3(
+            light.center.x - point.x, light.center.y - point.y, light.center.z - point.z
+        ).normalized()
+
+        light_intersects = []
+        light_normals = []
+        light_objs = []
+
+        for obj in objects:
+            intersect = obj.intersect(Ray(point, vec_to_light))
+            if intersect:
+                light_intersects.append(intersect)
+                light_normals.append(
+                    obj.get_normal(Ray(point, vec_to_light).get_point(intersect))
+                )
+                light_objs.append(obj)
+
+        if len(light_intersects) > 0:
+            light_index = light_intersects.index(min(light_intersects))
+            o = light_objs[light_index]
+            if isinstance(o, Light):
+                diffuse = max(0, vec_to_light.dot(normal))
+                factor = max(diffuse, 0.1)
+                color = (color[0] * factor, color[1] * factor, color[2] * factor)
+            else:
+                color = (color[0] * 0.1, color[1] * 0.1, color[2] * 0.1)
+
+        if isinstance(objs[index], Light):
+            return objs[index].color
+
+        reflected_ray = Ray(
+            Vector3(
+                point.x + normals[index].x,
+                point.y + normals[index].y,
+                point.z + normals[index].z,
+            ),
+            ray_direction,
+        ).reflected_ray(point, normal)
+        if max_depth > 0:
+            reflection_color = get_color(
+                pixel,
+                objects,
+                reflected_ray.origin + reflected_ray.direction.normalized().mult(0.001),
+                max_depth - 1,
+                light,
+            )
+            
+            reflection_color = Vector3(
+                reflection_color[0], reflection_color[1], reflection_color[2]
+            )
+            color = Vector3(color[0], color[1], color[2])
+            reflection_coefficient = 0.8
+            color = color.mult(1 - reflection_coefficient) + reflection_color.mult(
+                reflection_coefficient
+            )
+            color = (int(color.x), int(color.y), int(color.z))
+
+    return color
+
+
+def color_combine(color1, color2, depth=1):
+    return (
+        int((color1[0] + color2[0]) / 2),
+        int((color1[1] + color2[1]) / 2),
+        int((color1[2] + color2[2]) / 2),
+    )
+
+
 if __name__ == "__main__":
-    SCREEN_WIDTH = 640
-    SCREEN_HEIGHT = 640
-    MAX_DEPTH = 5
-    
     camera_position = Vector3(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 1000)
 
     # Initialize pygame
@@ -121,87 +217,33 @@ if __name__ == "__main__":
 
     screen.fill((0, 0, 0))
 
-    sphere1 = Sphere(Vector3(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, -1200), 350, (255, 0, 0))
+    sphere1 = Sphere(
+        Vector3(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, -1200), 350, (255, 0, 0)
+    )
     sphere2 = Sphere(Vector3(-300, 200, -1700), 800, (100, 0, 100))
-    light = Light(Vector3(SCREEN_WIDTH, 0, -1000), 300, (200, 200, 200))
+    light = Light(Vector3(SCREEN_WIDTH, 0, -500), 300, (255, 255, 255))
 
-    back_wall = Plane(Vector3(0, 0, -2000), Vector3(0, 0, 1), (0, 255, 0))
+    back_wall = Plane(Vector3(0, 0, -2000), Vector3(0, 0, 1), (255, 255, 255))
     left_wall = Plane(Vector3(-800, 0, 0), Vector3(1, 0, 0), (0, 0, 255))
-    bottom_wall = Plane(Vector3(0, SCREEN_HEIGHT + 800, 0), Vector3(0, -1, 0), (255, 255, 0))
+    bottom_wall = Plane(
+        Vector3(0, SCREEN_HEIGHT + 800, 0), Vector3(0, -1, 0), (255, 255, 255)
+    )
     top_wall = Plane(Vector3(0, -800, 0), Vector3(0, 1, 0), (255, 255, 255))
-    right_wall = Plane(Vector3(SCREEN_WIDTH + 800, 0, 0), Vector3(-1, 0, 0), (255, 0, 255))
+    right_wall = Plane(
+        Vector3(SCREEN_WIDTH + 800, 0, 0), Vector3(-1, 0, 0), (255, 0, 0)
+    )
 
-    objects = [sphere1, sphere2, light, back_wall, left_wall, bottom_wall, top_wall, right_wall]
+    objects = [
+        sphere1,
+        sphere2,
+        light,
+        back_wall,
+        left_wall,
+        bottom_wall,
+        top_wall,
+        right_wall,
+    ]
 
-    for y in range(SCREEN_HEIGHT):
-        for x in range(SCREEN_WIDTH):
-            intersects = []
-            normals = []
-            objs = []
-            color = (0, 0, 0)
-            ray_direction = get_direction_to((x, y), camera_position, SCREEN_WIDTH, SCREEN_HEIGHT)
-            for obj in objects:
-                intersect = obj.intersect(Ray(camera_position, ray_direction))
-                if intersect:
-                    intersects.append(intersect)
-                    normals.append(obj.get_normal(Ray(camera_position, ray_direction).get_point(intersect)))
-                    objs.append(obj)
-            
-            if len(intersects) > 0:
-                index = intersects.index(min(intersects))
-                intersect = intersects[index]
-                normal = normals[index]
-                point = Ray(camera_position, ray_direction).get_point(intersect)
-                color = objs[index].color
-            
-            screen.set_at((x, y), color)
-
-            # result_colors = []
-            # for i in range(4):
-            #     pixel = (x, y)
-            #     jostled_pixel = (x + i / 4, y + i / 4)
-            #     direction = get_direction_to(pixel, camera_position)
-            #     ray = Ray(camera_position, direction)
-            #     intersect = sphere.intersect(ray)
-            #     if intersect:
-            #         t = intersect
-            #         point = ray.get_point(t)
-            #         result_colors.append(sphere.color)
-            #         # color = sphere.color
-            #         # screen.set_at(pixel, color)
-            #         normal = sphere.get_normal(point)
-            #         reflected_ray = ray.reflected_ray(point, normal)
-            #         reflected_intersect = light.intersect(reflected_ray)
-            #         if reflected_intersect:
-            #             color = light.color
-            #             rgb = result_colors[-1]
-            #             result_colors.append(color)
-                        
-
-            
-            # if len(result_colors) > 0:
-            #     r = 0
-            #     g = 0
-            #     b = 0
-            #     for color in result_colors:
-            #         r += color[0]
-            #         g += color[1]
-            #         b += color[2]
-            #     r /= len(result_colors)
-            #     g /= len(result_colors)
-            #     b /= len(result_colors)
-            #     r = int(r)
-            #     g = int(g)
-            #     b = int(b)
-            #     r = r if r < 255 else 255
-            #     g = g if g < 255 else 255
-            #     b = b if b < 255 else 255
-            #     color = (r, g, b)
-            #     screen.set_at(pixel, color)
-                    
-
-            
-    
     pygame.display.flip()
 
     running = True
@@ -209,6 +251,16 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
+
+        for y in range(SCREEN_HEIGHT):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    pygame.quit()
+
+            pygame.display.flip()
+            for x in range(SCREEN_WIDTH):
+                color = get_color((x, y), objects, camera_position, MAX_DEPTH, light)
+                screen.set_at((x, y), color)
+
     pygame.quit()
-            
