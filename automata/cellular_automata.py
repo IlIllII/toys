09402bd57a038457dca34bf7d96ec9457a9a9d58
IlIllII <78166995+IlIllII@users.noreply.github.com>
2023-09-21 12:11:49 -0400

    Reorganize code for readability

--- automata/cellular_automata.py
+++ automata/cellular_automata.py
@@ -1,4 +1,3 @@
-from multiprocessing import process
 import pygame
 
 CELL_DIM = 10
@@ -8,51 +7,58 @@ MANUALLY_PLACED_COLOR = (100, 140, 250)
 BOARD_DIM = (100, 100)
 
 
-def process_cell(board, i, j):
-    left = board[i - 1][j - 1] == 1
-    middle = board[i][j - 1] == 1
-    right = board[i + 1][j - 1] == 1
+def update_cell_state(board, x, y):
+    above_left_alive = board[x - 1][y - 1] == 1
+    above_middle_alive = board[x][y - 1] == 1
+    above_right_alive = board[x + 1][y - 1] == 1
+    res = 0
 
-    if left and middle and right:
-        return 0
-    elif left and middle and not right:
-        return 0
-    elif left and not middle and right:
-        return 0
-    elif not left and not middle and not right:
-        return 0
+    if above_left_alive and above_middle_alive and above_right_alive:
+        res = 0
+    elif above_left_alive and above_middle_alive and not above_right_alive:
+        res = 0
+    elif above_left_alive and not above_middle_alive and above_right_alive:
+        res = 0
+    elif not above_left_alive and not above_middle_alive and not above_right_alive:
+        res = 0
     else:
-        return 1
+        res = 1
+
+    board[x][y] = res
 
 
 def update_board_state(board, manually_placed_tiles):
-    for i in range(BOARD_DIM[1]):
-        for j in range(BOARD_DIM[0]):
-            if i == 0 or j == 0 or i == BOARD_DIM[0] - 1 or j == BOARD_DIM[1] - 1:
+    for x in range(BOARD_DIM[1]):
+        for y in range(BOARD_DIM[0]):
+            if is_border_cell(x, y):
                 continue
             else:
-                board[i][j] = process_cell(board, i, j)
-                if (i, j) in manually_placed_tiles:
-                    board[i][j] = 1
+                update_cell_state(board, x, y)
+                if (x, y) in manually_placed_tiles:
+                    board[x][y] = 1
 
 
-def draw_cell(screen, i, j, color):
+def is_border_cell(x, y):
+    return x == 0 or y == 0 or x == BOARD_DIM[0] - 1 or y == BOARD_DIM[1] - 1
+
+
+def draw_cell(screen, x, y, color):
     pygame.draw.rect(
         screen,
         color,
-        (i * CELL_DIM, j * CELL_DIM, CELL_DIM, CELL_DIM),
+        (x * CELL_DIM, y * CELL_DIM, CELL_DIM, CELL_DIM),
     )
 
 
 def draw_board(screen, board, manually_placed_tiles):
-    for i in range(BOARD_DIM[1]):
-        for j in range(BOARD_DIM[0]):
-            if (i, j) in manually_placed_tiles:
-                draw_cell(screen, i, j, MANUALLY_PLACED_COLOR)
-            elif board[i][j] == 0:
-                draw_cell(screen, i, j, INACTIVE_COLOR)
+    for x in range(BOARD_DIM[1]):
+        for y in range(BOARD_DIM[0]):
+            if (x, y) in manually_placed_tiles:
+                draw_cell(screen, x, y, MANUALLY_PLACED_COLOR)
+            elif board[x][y] == 0:
+                draw_cell(screen, x, y, INACTIVE_COLOR)
             else:
-                draw_cell(screen, i, j, ACTIVE_COLOR)
+                draw_cell(screen, x, y, ACTIVE_COLOR)
 
 
 def process_events(board, manually_placed_tiles):
@@ -88,7 +94,6 @@ def main_loop(screen, board, manually_placed_tiles):
 
 def main():
     pygame.init()
-
     screen = pygame.display.set_mode((BOARD_DIM[0] * CELL_DIM, BOARD_DIM[1] * CELL_DIM))
     board = [[0 for i in range(BOARD_DIM[0])] for j in range(BOARD_DIM[1])]
     manually_placed_tiles = set()
